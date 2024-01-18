b98a90827b926a1bd599e8ecd8c863f80cc22222
Hansraj Singh Tomar <76831269+Hansraj-singh-tomar@users.noreply.github.com>
2024-01-18 14:27:28 +0530

    Update Accordion.jsx

--- src/Components/DesignAccordion/Accordion.jsx
+++ src/Components/DesignAccordion/Accordion.jsx
@@ -81,5 +81,33 @@ const AccordionCard = styled.div`
 `;
 
 
+// Accordion.jsx component
+// import React from 'react'
+// import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+
+// const Accordion = ({ val, showItem, toggleFun }) => {
+
+//     function handleToogle() {
+//         toggleFun();
+//     }
+
+//     return (
+//         <div className='mb-2'>
+//             <div onClick={handleToogle} className='bg-slate-500 p-2 text-white flex items-center justify-between rounded-lg cursor-pointer'>
+//                 <h1>{val?.title}</h1>
+//                 {showItem ? <IoIosArrowDown /> : <IoIosArrowUp />}
+//             </div>
+//             <div>
+//                 {
+//                     showItem && <p className='pl-4 text-indigo-950'>{val?.description}</p>
+//                 }
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Accordion
+
+
 
 
