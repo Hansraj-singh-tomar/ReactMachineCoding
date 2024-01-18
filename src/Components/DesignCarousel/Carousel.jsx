3ef3169bb773a7797978bca7d0a847d99107b8d1
Hansraj Singh Tomar <76831269+Hansraj-singh-tomar@users.noreply.github.com>
2024-01-18 14:14:18 +0530

    Update Carousel.jsx

--- src/Components/DesignCarousel/Carousel.jsx
+++ src/Components/DesignCarousel/Carousel.jsx
@@ -136,5 +136,16 @@ const CardCmp = styled.div`
 // export default App;
 
 // Card.jsx component
+// import React from 'react'
+
+// const Card = ({ index }) => {
+//     return (
+//         <div className='min-w-72 m-4 h-72 bg-black text-white rounded-lg flex justify-center items-center text-2xl font-bold'>
+//             <h1>Card - {index}</h1>
+//         </div>
+//     )
+// }
+
+// export default Card
 
     
